bd1971d4593d25d6b9672c4cb814c305754a3276
Aleksey Pecheritsa <fromzeya@gmail.com>
2023-05-07 11:16:37 +0700

    After 5-11

--- Module5-server/src/service/articleService.js
+++ Module5-server/src/service/articleService.js
@@ -1,8 +1,12 @@
+const NotImplementedError = require("../infrastructure/errors/NotImplementedError");
+
 module.exports = {
   getAllArticles: () => {
+    throw new NotImplementedError("Not implemented");
     return "All articles from service";
   },
   createNewArticle: (user) => {
+    throw new NotImplementedError("Not implemented");
     return "Create article from servive by " + user;
   },
 };
