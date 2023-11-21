bf21f94402e8d8fed17cccfdc69c6e84a8f48b75
Aleksey Pecheritsa <fromzeya@gmail.com>
2023-11-21 18:05:10 +0700

    Add lesson 6-8

--- Module6/src/Components/Layout.jsx
+++ Module6/src/Components/Layout.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import "./Layout.css";
+import { useState } from "react";
 
 const Layout = ({ children }) => {
+  const [active, setActive] = useState("all");
+
   return (
     <div>
       <nav>
-        <ul>
-          <li>All Articles</li>
-          <li>My Articles</li>
-          <li>Profile</li>
+        <ul style={{ display: "flex", flexDirection: "row" }}>
+          <li
+            style={{
+              listStyle: "none",
+              backgroundColor: active === "all" ? "red" : "white",
+            }}
+          >
+            All Articles
+          </li>
+          <li
+            style={{ listStyle: "none" }}
+            className={`${active === "my" ? "green" : ""}`}
+          >
+            My Articles
+          </li>
+          <li style={{ listStyle: "none" }}>Profile</li>
         </ul>
       </nav>
       {children}
